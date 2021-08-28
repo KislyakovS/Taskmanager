e9e38658f84359678cd8eaebacf5575f8d81eed6
Alexandr Kislyakov <kislyakov.sk@yandex.ru>
2021-08-28 10:31:38 +0300

    Added data filter

---
+++ src/mock/filter.js
@@ -0,0 +1,12 @@
+const filterMock = [
+    { name: 'all', count: 5 },
+    { name: 'overdue', count: 9 },
+    { name: 'today', count: 3 },
+    { name: 'favorites', count: 7 },
+    { name: 'repeating', count: 2 },
+    { name: 'archive', count: 6 }
+];
+
+export {
+    filterMock
+}
