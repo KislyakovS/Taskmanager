c181741ce46f666c46fb32868565a355cc1675de
Alexandr Kislyakov <kislyakov.sk@yandex.ru>
2021-09-07 19:15:13 +0300

    Integration sorted tasks

--- src/controller/board.js
+++ src/controller/board.js
@@ -1,4 +1,4 @@
-import { Sort } from '../components/sort';
+import { Sort, SortedTasks } from '../components/sort';
 import { Task } from '../components/task';
 import { LoadMoreButton } from '../components/load-more-button';
 
@@ -9,18 +9,40 @@ import { POSITION_INSERT } from '../const';
 class BoardController {
     constructor(component) {
         this._component = component;
+        this._container = this._component.element;
+
+        this._sortComponent = new Sort();
+        this._loadMoreComponent = new LoadMoreButton();
+
+        this._tasks = [];
     }
 
     render(tasks) {
-        const container = this._component.element;
-        const boardElement = container.querySelector('.board');
-        const taskListElement = container.querySelector('.board__tasks');
+        this._tasks = tasks;
 
-        const sortComponent = new Sort();
+        this._renderControl();
+        this._renderTasksList(tasks);
 
-        renderComponent(boardElement, sortComponent, POSITION_INSERT.AFTERBEGIN);
+        this._setEvents();
+    }
+
+    _renderControl() {
+        const boardElement = this._container.querySelector('.board');
+
+        renderComponent(boardElement, this._sortComponent, POSITION_INSERT.AFTERBEGIN);
+        renderComponent(boardElement, this._loadMoreComponent);
+    }
+
+    _renderTasksList(tasks) {
+        const taskListElement = this._container.querySelector('.board__tasks');
+
         tasks.forEach(task => renderComponent(taskListElement, new Task(task)));
-        renderComponent(boardElement, new LoadMoreButton());
+    }
+
+    _setEvents() {
+        this._sortComponent.setSortTypeChangeHandler((sortType) => {
+            const sortTasks = SortedTasks.sort(sortType, this._tasks);
+        });
     }
 }
 
