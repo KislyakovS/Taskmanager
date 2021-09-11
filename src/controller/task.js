01edd2b535406c1ab2bc10adc9b3cb190563ff60
Alexandr Kislyakov <kislyakov.sk@yandex.ru>
2021-09-11 09:58:57 +0300

    Added method set event task controller

--- src/controller/task.js
+++ src/controller/task.js
@@ -7,13 +7,31 @@ class TaskController {
         this._container = container;
 
         this._taskComponent = null;
+
+        this._onArchiveButtonClick = this._onArchiveButtonClick.bind(this);
+        this._onEditButtonClick = this._onEditButtonClick.bind(this);
+        this._onFavoritesButtonClick = this._onFavoritesButtonClick.bind(this);
     }
 
     render(task) {
         this._taskComponent = new Task(task);
 
         renderComponent(this._container, this._taskComponent);
+
+        this._setEvents();
     }
+
+    _setEvents() {
+        this._taskComponent.setArchiveButtonClickHandler(this._onArchiveButtonClick);
+        this._taskComponent.setEditButtonClickHandler(this._onEditButtonClick);
+        this._taskComponent.setFavoritesButtonClickHandler(this._onFavoritesButtonClick);
+    }
+
+    _onArchiveButtonClick() { }
+
+    _onEditButtonClick() { }
+
+    _onFavoritesButtonClick() { }
 }
 
 export {
