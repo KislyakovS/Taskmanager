08ca21164516f083c0af4f708a79bfd42db83958
Alexandr Kislyakov <kislyakov.sk@yandex.ru>
2021-09-17 19:56:36 +0300

    Update model tasks add filter

--- src/model/tasks.js
+++ src/model/tasks.js
@@ -1,17 +1,31 @@
-class Tasks {
+import { getTasksByFilter } from '../utils/filter';
+import { FILTER_TYPE } from '../const';
+
+class TasksModel {
     constructor() {
         this._tasks = [];
+        this._activeFilterType = FILTER_TYPE.ALL;
 
         this._dataChangeHandlers = [];
+        this._filterChangeHandlers = [];
     }
 
     get tasks() {
+        return getTasksByFilter(this._tasks, this._activeFilterType);
+    }
+
+    get tasksAll() {
         return this._tasks;
     }
 
     set tasks(tasks) {
         this._tasks = [...tasks];
         this._callHandlers(this._dataChangeHandlers);
+    }
+
+    set filter(type) {
+        this._activeFilterType = type;
+        this._callHandlers(this._filterChangeHandlers);
     }
 
     updateTask(id, newTask) {
@@ -32,11 +46,15 @@ class Tasks {
         this._dataChangeHandlers.push(handler);
     }
 
+    set filterChangeHandlers(handler) {
+        this._filterChangeHandlers.push(handler);
+    }
+
     _callHandlers(handlers) {
         handlers.forEach(handler => handler());
     }
 }
 
 export {
-    Tasks
+    TasksModel
 }
