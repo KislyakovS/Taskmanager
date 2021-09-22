c1e81fd035edb873eb0fcc70b2e1d6c1edf89b7a
Alexandr Kislyakov <kislyakov.sk@yandex.ru>
2021-09-22 21:56:43 +0300

    Added class API

---
+++ src/api.js
@@ -0,0 +1,25 @@
+import { FETCH_URL } from './const';
+
+class API {
+    constructor(authorization) {
+        if (!authorization) {
+            throw new Error('Error during authorization');
+        }
+
+        this._authorization = authorization;
+    }
+
+    get _headers() {
+        const header = new Headers();
+
+        header.append('Authorization', this._authorization);
+
+        return header;
+    }
+
+    getTasks() {
+        return fetch(`${FETCH_URL}/tasks`, {
+            headers: this._headers
+        }).then(result => result.json());
+    }
+}
