f1184d562aa96afa42f626dfd21358c12dc39aba
Alexandr Kislyakov <kislyakov.sk@yandex.ru>
2021-09-23 22:14:14 +0300

    Integration api main script

--- src/main.js
+++ src/main.js
@@ -6,25 +6,32 @@ import { TasksModel } from './model/tasks';
 import { BoardController } from './controller/board';
 import { FilterController } from './controller/filter';
 
-import { generateTasks } from './mock/task';
-
 import { renderComponent } from './utils';
 
-const TASK_COUNT = 8;
+import { API } from './api';
 
 const siteMainElement = document.querySelector('.main');
 const siteHeaderElement = siteMainElement.querySelector('.main__control');
 
 renderComponent(siteHeaderElement, new SiteMenu());
 
-const tasksModel = new TasksModel();
-tasksModel.tasks = generateTasks(TASK_COUNT);
+const STRING_AUTH = 'Basic eo0w590ik29889a';
+const api = new API(STRING_AUTH);
 
-const filterController = new FilterController(siteMainElement, tasksModel);
-filterController.render();
+api.getTasks()
+    .then((tasks) => {
+        const tasksModel = new TasksModel();
+        tasksModel.tasks = tasks;
 
-const boardComponent = new Board();
-const boardController = new BoardController(boardComponent, tasksModel);
-renderComponent(siteMainElement, boardComponent);
-boardController.render();
+        const filterController = new FilterController(siteMainElement, tasksModel);
+        filterController.render();
 
+        const boardComponent = new Board();
+        const boardController = new BoardController(boardComponent, tasksModel);
+        renderComponent(siteMainElement, boardComponent);
+        boardController.render();
+    })
+    .catch((error) => {
+        console.error(error);
+        document.body.innerHTML = 'Ошибка при загрузке данных';
+    });
